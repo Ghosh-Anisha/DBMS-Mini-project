3e16815a1232974067eeb91bd7b6a436ef6e9852
Aanchal Narendran <aanchalnarendran@Aanchals-MacBook-Air.local>
2021-10-25 16:12:32 +0530

    Modified insert and ddl files

--- EZBookddl.sql
+++ EZBookddl.sql
@@ -5,27 +5,27 @@ create database ezbook;
 
 create table Books (
 	ISBN VARCHAR(10) NOT NULL UNIQUE,
-	BookID INT UNIQUE,
+	BookID VARCHAR UNIQUE,
 	BName VARCHAR,
 	Genre VARCHAR(100),
 	Author VARCHAR,
 	PublishingYear int,
 	Price DECIMAL(7,2),
 	Rating DECIMAL(3,1),
-	PRIMARY KEY (ISBN, BookID)
-);
+	PRIMARY KEY (ISBN)
+); --imputed
 
 create table PublicationHouse (
-	BookID int unique not null references Books(BookID),
+	BookID VARCHAR unique not null references Books(BookID),
 	Publication_House VARCHAR not null,
-	ISBN VARCHAR(10) references Books(ISBN),
-	PRIMARY KEY (BookID,Publication_House)
+	ISBN VARCHAR(10) NOT NULL UNIQUE,
+	PRIMARY KEY (ISBN) --imputed
 );
 
 create table Produces(
-	ISBN VARCHAR(10) references Books(ISBN),
-	Publication_House VARCHAR references PublicationHouse(Publication_House),
-	PRIMARY KEY(ISBN,Publication_House)
+	ProducedBookID VARCHAR references Books(BookID),
+	PublicationISBN VARCHAR references PublicationHouse(ISBN),
+	PRIMARY KEY(ProducedBookID,PublicationISBN) --imputed
 );
 
 create table Customer(
@@ -33,35 +33,36 @@ create table Customer(
 	CustName VARCHAR(20),
 	CustNumber VARCHAR(10),
 	EmailID VARCHAR(40)
-);
+); --imputed
 
 create table RequestForBooks(
 	RequestID INT,
 	Book_title VARCHAR,
 	CustomerID INT references Customer(CustomerID),
-	ISBN VARCHAR(10) references Books(ISBN),
-	PRIMARY KEY(RequestID, CustomerID)
-);
+	PRIMARY KEY(RequestID)
+); --imputed 
 
 create table Buys(
 	CustomerID INT not null references Customer(CustomerID),
-	ISBN VARCHAR(10) unique references Books(ISBN),
-	PRIMARY KEY(CustomerID, ISBN)
-);
+	ISBN VARCHAR(10) references Books(ISBN),
+	PurchaseId INT UNIQUE,
+	PRIMARY KEY(PurchaseId)
+); --imputed
 
 create table CustomerFeedback(
 	CustomerID INT references Customer(CustomerID),
 	CustDesc VARCHAR,
 	ISBN VARCHAR(10) references Books(ISBN),
 	PRIMARY KEY(CustomerID, ISBN)
-);
+); --imputed
 
 create table Cart(
 	CustomerID INT references Customer(CustomerID),
-	CartID INT PRIMARY KEY,
-	BookID INT references Books(BookID),
-	Book_Count INT
-);
+	CartID INT unique ,
+	BookID VARCHAR references Books(BookID),
+	Book_Count INT,
+	PRIMARY KEY (CartID)
+); --imputed
 
 create table Bills(
 	BillNumber INT PRIMARY KEY,
@@ -69,16 +70,16 @@ create table Bills(
 	BillDate DATE,
 	TotalAmount DECIMAL(15,2),
 	ModeOfPayment VARCHAR
-);
+); --imputed
 
 create table BestSellers(
 	BillNumber INT references Bills(BillNumber),
-	ISBN VARCHAR(10) references Buys(ISBN),
-	PRIMARY KEY (BillNumber, ISBN)
-);
+	PurchaseId int references Buys(PurchaseId),
+	PRIMARY KEY (BillNumber, PurchaseId)
+); --imputed
 
 create table CountBooks(
 	BillNumber INT references Bills(BillNumber),
-	ISBN VARCHAR(10) references Books(ISBN) PRIMARY KEY
-);
+	ISBN VARCHAR(10) references Books(ISBN)
+); --imputed 
 
