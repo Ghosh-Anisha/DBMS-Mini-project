a444a18b1ce005a028f0d4ce5dcc1cb6bbc77d1b
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-10-25 23:34:54 +0530

    Add files via upload

--- permissions.sql
+++ permissions.sql
@@ -1,11 +1,11 @@
 \c ezbook
 
-create user customer with nosuperuser;
-grant connect on database "ezbook" to customer;
-grant select on BestSellers to customer;
-grant select on Bills to customer;
-grant select on Cart to customer;
-grant update on Cart to customer;
+create user cust with nosuperuser;
+grant connect on database "ezbook" to cust;
+grant select on BestSellers to cust;
+grant select on Bills to cust;
+grant select on Cart to cust;
+grant update on Cart to cust;
 
 create user cashier with nosuperuser;
 grant connect on database "ezbook" to cashier;
@@ -26,3 +26,36 @@ grant select on bills to admin;
 grant update on bills to admin;
 grant select on cart to admin;
 grant delete on cart to admin;
+
+grant select on customer to admin;
+grant delete on customer to admin;
+grant update on customer to admin;
+grant select on customer to cust;
+grant update on customer to cust;
+
+grant select on buys to admin;
+
+grant select on customerfeedback to admin;
+grant select on customerfeedback to cust;
+grant update on customerfeedback to cust;
+grant delete on customerfeedback to cust;
+
+grant select on books to admin;
+grant update on books to admin;
+grant delete on books to admin;
+grant select on books to cust;
+
+grant select on produces to admin;
+grant update on produces to admin;
+grant delete on produces to admin;
+
+grant select on PublicationHouse to admin;
+grant update on PublicationHouse to admin;
+grant delete on PublicationHouse to admin; 
+grant select on PublicationHouse to cust;
+
+grant select on RequestForBooks to admin;
+grant delete on RequestForBooks to admin;
+grant select on RequestForBooks to cust;
+grant update on RequestForBooks to cust;
+
